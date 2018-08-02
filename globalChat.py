a48c7bb0d39b56038d6f77e782a246a540f663f0
obwan02 <obwan02@hotmail.co.nz>
2018-08-03 10:30:51 +1200

    Add files via upload

--- globalChat.py
+++ globalChat.py
@@ -2,8 +2,7 @@ import socket as s
 import threading
 import time
 import os
-
-send = False
+import tkinter as tk
 
 read = s.socket(s.AF_INET, s.SOCK_DGRAM)
 read.bind(('', 2222))
@@ -14,26 +13,41 @@ write.setsockopt(s.SOL_SOCKET, s.SO_BROADCAST, 1)
 IP = s.gethostbyname(os.environ['COMPUTERNAME'])
 
 def listenThread():
+    global text
     while True:
         msg, add = read.recvfrom(1024)
         ip, port = add
         if not ip == IP:
-            print(msg.decode('utf-8'))
+            t = msg.decode('utf-8')
+            text.configure(state="normal")
+            text.insert(tk.END, t + '\n')
+            text.configure(state="disabled")
 
-rThread = threading.Thread(target=listenThread)
-
-name = input('Please specify your name: ').strip()
-print('To exit the chat type:(EXIT)')
-time.sleep(1)
-os.system('cls')
-
-rThread.start()
-while True:
-    text = input('>')
-    if text == '(EXIT)':
-        break
+def sendMessage(text):
+    global writer, root
+    writer.delete(0, tk.END)
+    if text == '/EXIT':
+        write.close()
+        try:
+            read.close()
+        except OSError:
+            pass
+        root.destroy()
+        exit(2)
+        
     write.sendto(bytes(name + ": " + text, 'utf-8'), ('255.255.255.255', 2222))
 
-read.close()
-write.close()
+rThread = threading.Thread(target=listenThread)
+name = input('Please specify your name: ').strip()
+print('To exit the chat type:/EXIT')
+rThread.start()
+
+root = tk.Tk()
+text = tk.Text(root)
+text.configure(state="disabled")
+text.grid(column=0, row=0)
+writer = tk.Entry(root, width=100)
+writer.bind('<Return>', lambda e: sendMessage(writer.get()))
+writer.grid(column=0, row=1)
+root.mainloop()
     
