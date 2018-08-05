aa2dc4619277c72888cf35bcc581f55d9eb495ce
obwan02 <obwan02@hotmail.co.nz>
2018-08-06 11:58:36 +1200

    Add files via upload

--- globalChat.py
+++ globalChat.py
@@ -14,56 +14,91 @@ IP = s.gethostbyname(os.environ['COMPUTERNAME'])
 
 people = []
 
-def listenThread():
+def addText(txt):
     global text
+    text.configure(state="normal")
+    text.insert(tk.END, txt + '\n')
+    text.configure(state="disabled")
+    text.see("end")
+
+def listenThread():
+    global text, people
     while True:
-        msg, add = read.recvfrom(1024)
+
+        msg, add = (None, None)
+        try:    
+            msg, add = read.recvfrom(1024)
+        except OSError:
+            pass
         msg = msg.decode('utf-8')
         ip, port = add
+
+        print(msg)
 
         if msg.startswith('\r'):
             #Handle as a request
             if msg.startswith('\rGET PEOPLE;'):
                 p = msg[msg.find(';') + 1:]
-                people.append(p)
+                
+                people.append((p, ip))
+                updatePeople()
+                
                 print(p + " has connected.\n")
+                addText(p + " has connected.")
                 write.sendto(b'\rSEND NAME;' + bytes(name, 'utf-8'), (ip, 2222))
             if msg.startswith('\rSEND NAME;'):
                 if not ip == IP:
-                    people.append(msg[msg.find(';') + 1:])
-                    print(ip)
+                    people.append((msg[msg.find(';') + 1:], ip))
+                    updatePeople()
             if msg.startswith('\rDEL NAME;'):
                 try:
-                    people.remove(msg[msg.find(';') + 1:])
+                    people.remove((msg[msg.find(';') + 1:], ip))
+                    updatePeople()
                 except ValueError:
                     print('Bad Name Value')
         else: 
-            text.configure(state="normal")
-            text.insert(tk.END, msg + '\n')
-            text.configure(state="disabled")
-            text.see("end")
+            addText(msg)
+
+
+lastSendTime = 0
 
 def sendMessage(text):
-    global writer, root
+    global writer, root, people, lastSendTime
+
+    if text.strip() == "":
+        print("Spam message detected")
+        return
+    if time.time() - lastSendTime < 1:
+        print('Sending messages too quickly')
+        lastSendTime = time.time()
+        return
+    
     writer.delete(0, tk.END)
     if text == '/EXIT':
-
-        try:
-            read.close()
-        except OSError:
-            print('Failed to close read port')
         
-        write.sendto(bytes(name + " disconnected", 'utf-8'), ('255.255.255.255', 2222))
-        write.sendto(bytes('\rDEL NAME;' + name, 'utf-8'), ('255.255.255.255', 2222))
-        write.close()
-        
+        disconnect()
         root.destroy()
-        exit(2)
+        exit(0)
         
     write.sendto(bytes(name + ": " + text, 'utf-8'), ('255.255.255.255', 2222))
 
+def disconnect():
+    try:
+        read.close()
+    except OSError:
+        print('Failed to close read port')
+        
+    write.sendto(bytes(name + " disconnected", 'utf-8'), ('255.255.255.255', 2222))
+    write.sendto(bytes('\rDEL NAME;' + name, 'utf-8'), ('255.255.255.255', 2222))
+    write.close()
 
+def updatePeople():
+    global people, peopleList
 
+    peopleList.delete(0, tk.END)
+
+    for i in people:
+        peopleList.insert(tk.END, i[0] + " - " + i[1])
 
 
 rThread = threading.Thread(target=listenThread)
@@ -75,14 +110,20 @@ while name == "":
 print('To exit the chat type:/EXIT')
 
 root = tk.Tk()
+root.protocol("WM_DELETE_WINDOW", disconnect)
 root.resizable(False, False)
 root.title('NBHS Chat')
+
 text = tk.Text(root)
 text.configure(state="disabled")
 text.grid(column=0, row=0)
+
+peopleList = tk.Listbox(root)
+peopleList.grid(column=1, row=0)
+
 writer = tk.Entry(root, width=100)
 writer.bind('<Return>', lambda e: sendMessage(writer.get()))
-writer.grid(column=0, row=1)
+writer.grid(column=0, row=1, rowspan=2)
 
 write.sendto(b'\rGET PEOPLE;' + bytes(name, 'utf-8'), ('255.255.255.255', 2222))
 rThread.start()
